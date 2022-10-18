6cb9c6aa395a34d0ca6416f0f6cca8330400a8b9
Felipe Takaoka <felipe.takaoka@nubank.com.br>
2022-10-18 09:01:01 -0300

    feat: Serve JSON on a specific route

--- back-end-development-and-apis/boilerplate-express/myApp.js
+++ back-end-development-and-apis/boilerplate-express/myApp.js
@@ -1,11 +1,15 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
-app.get("/", (req, res) => {
+app.get("/", (_, res) => {
   const path = __dirname + "/views/index.html";
   res.sendFile(path);
 });
 
 app.use("/public", express.static(__dirname + "/public"));
+
+app.use("/json", (req, res) => {
+  res.send({ message: "Hello json" });
+});
 
 module.exports = app;
