b4807cf3262a78e8c7172e533e6bbe0fccc02392
Felipe Takaoka <felipe.takaoka@nubank.com.br>
2022-10-22 19:04:18 -0300

    feat: Remove many people

--- back-end-development-and-apis/boilerplate-mongomongoose/myApp.js
+++ back-end-development-and-apis/boilerplate-mongomongoose/myApp.js
@@ -62,8 +62,7 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
-
-  done(null /*, data*/);
+  Person.remove({ name: nameToRemove }, done);
 };
 
 const queryChain = (done) => {
