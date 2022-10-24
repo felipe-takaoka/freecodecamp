fb24829eff2919e8462a33cbef6b1f8749fe2726
Felipe Takaoka <felipe.takaoka@nubank.com.br>
2022-10-24 10:17:20 -0300

    feat: Post user to db

--- back-end-development-and-apis/projects/boilerplate-project-exercisetracker/index.js
+++ back-end-development-and-apis/projects/boilerplate-project-exercisetracker/index.js
@@ -1,13 +1,48 @@
+require("dotenv").config();
+const bodyParser = require("body-parser");
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config();
+const mongoose = require("mongoose");
 
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+let userSchema = new mongoose.Schema({
+  username: { type: String, unique: true , required: true },
+})
+
+let exerciseSchema = new mongoose.Schema({
+  username: { type: String, required: true },
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: Date, required: true },
+});
+
+let User = mongoose.model("User", userSchema);
+let Exercise = mongoose.model("Exercise", exerciseSchema);
+
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
+
+app.post("/api/users", (req, res) => {
+  console.log(req);
+  const username = req.body.username;
+  const newUser = new User({ username });
+  newUser.save((err, doc) => {
+    if (err) {
+      res.json(err);
+    } else {
+      res.json({ username: doc.username, id: doc._id });
+    }
+  })
+})
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
