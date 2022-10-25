9d80750a9c476eeeed13c9171e651a6ce785f74f
Felipe Takaoka <felipe.takaoka@nubank.com.br>
2022-10-25 09:49:32 -0300

    feat: Post exercise

--- back-end-development-and-apis/projects/boilerplate-project-exercisetracker/index.js
+++ back-end-development-and-apis/projects/boilerplate-project-exercisetracker/index.js
@@ -15,7 +15,7 @@ let userSchema = new mongoose.Schema({
 });
 
 let exerciseSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  user_id: { type: String, required: true },
   description: { type: String, required: true },
   duration: { type: Number, required: true },
   date: { type: Date, required: true },
@@ -58,6 +58,23 @@ app
       }
     });
   });
+
+app.post("/api/users/:_id/exercises", (req, res) => {
+  const user_id = req.params._id;
+  const date = req.body.date ? new Date(req.body.date) : new Date();
+  const { description, duration } = req.body;
+  const newExercise = new Exercise({ user_id, description, duration, date });
+
+  User.findById(user_id, null, null, (err, _) => {
+    if (err) {
+      res.send(`Error finding user ${user_id}`)
+    } else {
+      newExercise.save((err, doc) => {
+        res.send(doc);
+      });
+    }
+  });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log("Your app is listening on port " + listener.address().port);
