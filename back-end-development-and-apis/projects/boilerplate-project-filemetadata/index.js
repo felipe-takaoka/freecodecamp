6371af62b3151c9195dd14714eaf9fd57dee3dbd
Felipe Takaoka <felipe.takaoka@nubank.com.br>
2022-10-29 19:02:38 -0300

    feat: Add endpoint to submit file

--- back-end-development-and-apis/projects/boilerplate-project-filemetadata/index.js
+++ back-end-development-and-apis/projects/boilerplate-project-filemetadata/index.js
@@ -1,20 +1,30 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-
-
+app.post(
+  "/api/fileanalyse",
+  upload.single("upfile"),
+  (req, res, next) => {
+    // req.file is the `upfile` file
+    // req.body will hold the text fields, if there were any
+    const { originalname, size, mimetype } = req.file;
+    res.json({ name: originalname, size, type: mimetype });
+  }
+);
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
